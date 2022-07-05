09b4b99ae4badd90c504058f908cd64a192f9561
zqc <1820901097@qq.com>
2022-07-05 22:54:46 +0800

    项目初始结构1

---
+++ mall-memeber/src/main/java/com/zqc/mall/mallmemeber/MallMemeberApplication.java
@@ -0,0 +1,13 @@
+package com.zqc.mall.mallmemeber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMemeberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMemeberApplication.class, args);
+    }
+
+}
---
+++ mall-memeber/src/test/java/com/zqc/mall/mallmemeber/MallMemeberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqc.mall.mallmemeber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemeberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
